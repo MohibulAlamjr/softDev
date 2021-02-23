acb9f1674449a783a116a5720551e385f215bcff
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-02-23 15:24:37 +0600

    Today1

---
+++ sweety/Javascript 1st day/asset/js/main.js
@@ -0,0 +1,75 @@
+var fname,lname,fullname,gender;
+
+fname='Shovhana';
+lname='Islam';
+gender='Female';
+
+if(gender=='Female'){
+
+    fullname= 'Miss. '+fname+' '+ lname;
+}
+else{
+    fullname= 'Mr. '+fname+' '+ lname;
+}
+console.log(gender);
+
+
+//for js syntex
+var x,y,z;
+x=10;
+y=20;
+z=x+y;
+console.log(z);
+
+var a=15,b='sweety',c=5,d=20;
+e=a+c+d+b;
+f=a+b+c+d;
+console.log(e);
+console.log(f);
+
+//for undefined
+
+var ud;
+typeof(ud);
+var ud=13;
+console.log(ud);
+
+{
+let n=00;
+n=3;
+console.log(n);
+}
+
+//difference of let and const
+
+const rd=25;
+console.log(rd);
+
+const s=5;
+const w=6;
+const t=s+w;
+console.log(t);
+
+{
+    let i=9;
+    i=19;
+    i=i+i;
+    console.log(i);
+}
+
+//Operator
+
+var a=3,b=2,c=10,g=21;
+d=a*b;
+m=c/b;
+i=g%b;
+console.log(d);
+console.log(m);
+console.log(i);
+
+
+
+
+
+
+
