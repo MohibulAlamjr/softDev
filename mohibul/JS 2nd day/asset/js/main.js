81f26e56f64f5f64e763696fef57e2cece614117
Mohibul Alam <mohibulalamjr@gmail.com>
2021-02-28 23:45:12 +0600

    2nd day

---
+++ mohibul/JS 2nd day/asset/js/main.js
@@ -0,0 +1,27 @@
+//  variable 
+
+var a=5;
+let b=10;
+const pi =32;
+
+if(a==5){
+    var sum;
+    let b=90;
+    const p=10;
+    sum=a+b;
+   
+}
+
+console.log(sum);
+
+var x= true;
+//boolean 
+//number
+
+
+console.log(typeof(x));
+
+// string
+// number
+// boolean
+// undefined
