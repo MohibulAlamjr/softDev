65cdc1e3ecc7f46936abb4f38193b3a30e011b48
Muniramouly <muniramouly25@gmail.com>
2021-03-03 23:44:25 +0600

    Solve commit5

---
+++ mouly/ProblemSolve1/asset/js/main9.js
@@ -0,0 +1,36 @@
+console.log('Problem number 09');
+
+var NUMBER,AMOUNT,HOURS,SALARY;
+
+NUMBER=25;
+AMOUNT=100;
+HOURS=5.50;
+
+console.log("NUMBER = " + NUMBER);
+
+SALARY=AMOUNT*HOURS;
+
+console.log('SALARY = ' + 'U$' +  ' ' +  SALARY);
+
+
+var NUMBER1,AMOUNT1,HOURS1,SALARY;
+NUMBER1=1;
+AMOUNT1=200;
+HOURS1=20.50;
+
+console.log("NUMBER1 = " + NUMBER1);
+
+SALARY=AMOUNT1*HOURS1;
+
+console.log('SALARY = ' + 'U$' +  ' ' +  SALARY);
+
+var NUMBER2,AMOUNT2,HOURS2,SALARY;
+NUMBER2=6;
+AMOUNT2=145;
+HOURS2=15.55;
+
+console.log("NUMBER2 = " + NUMBER2);
+
+SALARY=AMOUNT2*HOURS2;
+
+console.log('SALARY = ' + 'U$' +  ' ' +  SALARY);
