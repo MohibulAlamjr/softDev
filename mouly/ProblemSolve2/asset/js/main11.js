e8b1f28d7473b52d184a885e8e7b0f84b2d6da42
Muniramouly <muniramouly25@gmail.com>
2021-03-07 23:57:12 +0600

    commit6

---
+++ mouly/ProblemSolve2/asset/js/main11.js
@@ -0,0 +1,17 @@
+console.log('Problem number 11');
+
+var code,units,price;
+
+code=12;
+units=1;
+price=5.30;
+
+
+var code1,units1,price1;
+code1=16;
+units1=2;
+price1=5.10;
+var Taka=units1*price1;
+
+console.log("updatedprice = " + Taka);
+
