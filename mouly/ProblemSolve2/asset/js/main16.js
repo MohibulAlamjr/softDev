97448c25041ab6ce7334ec8f8e5ce6f71fbed1d2
Muniramouly <muniramouly25@gmail.com>
2021-03-11 10:53:54 +0600

    ProblemSolvecommit2

---
+++ mouly/ProblemSolve2/asset/js/main16.js
@@ -0,0 +1,49 @@
+console.log('Problem number 16');
+
+var X1,X2,Y1,Y2;
+
+X1=1.0;
+Y1=7.0;
+X2=5.0;
+Y2=9.0;
+
+var distance=Math.sqrt(((5-1)*(5-1))+((9-7)*(9-7)));
+console.log(distance );
+
+var X3,X4,Y3,Y4;
+
+X3=-2.5;
+Y3=0.4;
+X4=12.1;
+Y4=7.3;
+
+var distance=Math.sqrt(((12.1+2.5)*(12.1+2.5))+((7.3-0.4)*(7.3-0.4)));
+console.log(distance );
+
+
+var X5,X6,Y5,Y6;
+
+X5=2.5;
+Y5=-0.4;
+X6=-12.2;
+Y6=7.0;
+
+var distance=Math.sqrt(((-12.2-2.5)*(-12.2-2.5))+((7+0.4)*(7+0.4)));
+console.log(distance );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
