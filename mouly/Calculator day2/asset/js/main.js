15b2b8fb47d6732ec5ddd9aa2ac1adb10631ce75
Muniramouly <muniramouly25@gmail.com>
2021-03-13 21:06:42 +0600

    Calculator commit1

---
+++ mouly/Calculator day2/asset/js/main.js
@@ -0,0 +1,6 @@
+function calculator (b,c){
+    console.log("The summation is:" + (b+c) + ' ' + "The substraction is:" +(b-c)+ ' ' + "The multiplication is:" +(b*c)+ ' ' + "The division is:" +(b/c)+ ' ' + "The modulous is:" +(b%c));
+}
+calculator(12,15);
+
+ 
