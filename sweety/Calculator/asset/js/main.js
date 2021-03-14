b3f0fa868a6b06e73401bb138e03062b81b4e2a0
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-03-14 12:50:06 +0600

    uri

--- sweety/Calculator/asset/js/main.js
+++ sweety/Calculator/asset/js/main.js
@@ -1,11 +1,11 @@
 console.log('CALCULATOR');
 
 function calculator(a,b){
-    console.log('The summation is',' ',a+b);
-    console.log('The substraction is',' ',a-b);
-    console.log('The multiplication is',' ',a*b);
-    console.log('The division is',' ',a/b);
-    console.log('The modulation is',' ',a%b);
+    console.log('The summation is'+' '+a+b);
+    console.log('The substraction is'+' '+a-b);
+    console.log('The multiplication is'+' '+a*b);
+    console.log('The division is'+' '+a/b);
+    console.log('The modulation is'+' '+a%b);
 }
 calculator(20,11);
 
@@ -15,6 +15,23 @@ calculator(20,11);
 console.log('CALCULATOR=2');
 
 function calculator2(a,b){
-    console.log('The summation is',' ',a+b,',','The substraction is',' ',a-b,',','The multiplication is',' ',a*b,',','The division is',' ',a/b,',','The modulation is',' ',a%b);
+    console.log('The summation is'+' '+a+b +'The substraction is'+' '+a-b +'The multiplication is'+' '+a*b + 'The division is'+' '+ a/b +'The modulation is'+' '+a%b);
 }
 calculator2(20,11);
+
+
+// More One
+
+function calcu(a,b,c){
+    c=a+b;
+    console.log('The summation is'+' '+c);
+    c=a-b;
+    console.log('The substraction is'+' '+c);
+    c=a*b;
+    console.log('The multiplication is'+' '+c);
+    c=a/b;
+    console.log('The division is'+' '+c);
+    c=a%b;
+    console.log('The modulation is'+' '+c);
+}
+calcu(3,4);
---
+++ sweety/URI Prblm/asset/js/main19.js
@@ -0,0 +1,33 @@
+console.log('Problem = 19');
+
+
+var i=576;
+console.log(i);
+console.log(5 + ' nota(s)'+ ' ' + 'de R$ 100');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 50');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 20');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 10');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 5');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 2');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 1');
+
+var i=11257;
+console.log(i);
+console.log(112 + ' nota(s)'+ ' ' + 'de R$ 100');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 50');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 20');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 10');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 5');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 2');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 1');
+
+
+var i=503;
+console.log(i);
+console.log(112 + ' nota(s)'+ ' ' + 'de R$ 100');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 50');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 20');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 10');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 5');
+console.log(1 + ' nota(s)'+ ' ' + 'de R$ 2');
+console.log(0 + ' nota(s)'+ ' ' + 'de R$ 1');
