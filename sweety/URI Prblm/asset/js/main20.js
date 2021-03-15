741e0ea345e795ede36ae57e63f60e2cee6fe58f
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-03-16 03:28:11 +0600

    ok

---
+++ sweety/URI Prblm/asset/js/main20.js
@@ -0,0 +1,24 @@
+console.log('Problem = 20');
+
+var H,M,S,N=556;
+H=N/3600;
+M=N%3600/60;
+S=N%60;
+console.log(H + ' '+ ':' +' '+ M +' '+ ':' +' '+ S);
+
+
+var H,M,S,N=1;
+H=N/3600;
+M=N%3600/60;
+S=N%60;
+console.log(H + ' '+ ':' +' '+ M +' '+ ':' +' '+ S);
+
+
+var H,M,S,N=140153;
+H=N/3600;
+M=N%3600/60;
+S=N%60;
+console.log(H + ' '+ ':' +' '+ M +' '+ ':' +' '+ S);
+
+
+
