4e93ebe3143a767dcab416e69c14cf28b929a4fe
Mohibul Alam <mohibulalamjr@gmail.com>
2021-03-18 22:44:28 +0600

    home work function 4

---
+++ mohibul/Function 4/asset/js/main.js
@@ -0,0 +1,6 @@
+// acount the element and console element and elements number 
+// var x=  findOdd(1,2,3,5,7,8,9,80,81,96,89)
+// var y=  findEven(1,2,3,5,7,8,9,80,81,96,89)
+// var z=  findPrime(55,230)
+
+
