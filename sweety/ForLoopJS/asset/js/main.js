cac3125942acded4b093ee354ebe04e6d3688e7d
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-03 23:38:08 +0600

    hoo

--- sweety/ForLoopJS/asset/js/main.js
+++ sweety/ForLoopJS/asset/js/main.js
@@ -5,3 +5,22 @@ for (i = 0; i < cars.length; i++) {
   text += cars[i] + "\n";
 }
 console.log(text);
+
+
+var text = "";
+var i;
+for (i = 0; i < 5; i++) {
+  text += "The number is " + i + "\n";
+}
+console.log(text);
+
+
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 2;
+var len = cars.length;
+var text = "";
+
+for (; i < len; i++) {
+  text += cars[i] + "\n";
+}
+console.log(text);
