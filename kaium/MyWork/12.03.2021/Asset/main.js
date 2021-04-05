37f1c60f6a13cc39c290779179fd9fede95a08b0
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-05 22:22:38 +0600

    aa

---
+++ kaium/MyWork/12.03.2021/Asset/main.js
@@ -0,0 +1,20 @@
+function cal(a,b){
+    
+    console.log ("this is Sum  :", a+b);
+    
+    console.log("this is Subtraction  :",a-b);
+    
+    console.log("this is Multiplication  :",a*b);
+    
+    console.log("this is Divisin  :",a/b);
+    console.log("this is Modulus  :",a%b);
+
+    
+}
+cal(12,4);
+// input 12,4;
+// the sum is 16
+// the subtraction is 8.
+// the Multiplication is 48
+// the divisin 3
+// the modulus is 0
