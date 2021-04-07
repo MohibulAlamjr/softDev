41911c4516770f816435dd4278cade8cf246531a
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-08 05:10:47 +0600

    por

--- mohibul/Portfolio/asset/js/main.js
+++ mohibul/Portfolio/asset/js/main.js
@@ -1,1 +1,17 @@
-console.log('hi');
+$(function() {
+  
+    // contact form animations
+    $('#contact').click(function() {
+      $('#contactForm').fadeToggle();
+    })
+    $(document).mouseup(function (e) {
+      var container = $("#contactForm");
+  
+      if (!container.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+          container.fadeOut();
+      }
+    });
+    
+  });
