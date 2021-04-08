d5cac839cf1379158935f27732baad67cd11f19e
Muniramouly <muniramouly25@gmail.com>
2021-04-08 07:04:53 +0600

    portfolio1

--- mouly/String/asset/js/main.js
+++ mouly/String/asset/js/main.js
@@ -73,10 +73,16 @@ var a = 'BelyWhiteRoseBlueSunflowerYellow';
 var b = a.slice(9,13);
 var b1 = a.slice(13,17);
 var b2 = a.slice(5,8);
-var b3 = a.slice(-9,-11);
-var b4 = a.slice(-6,-7);
-var b5 = a.slice(-3,-5);
+var b3 = a.slice(-11,-9);
+var b4 = a.slice(-7,-6);
+var b5 = a.slice(-5,-3);
 
 
 console.log(b+b1+b2+b3+b4+b5);
+
+var fruits =["Banana","Orange","Apple"];
+var res = fruits.unshits("Jackfruit,Lemon");
+console.log(res);
+
+
 
