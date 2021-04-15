9a70e1a62653730c183fb83b30667b4cf204eb06
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-15 22:23:36 +0600

    loop

--- kaium/For Loop/asset/js/main.js
+++ kaium/For Loop/asset/js/main.js
@@ -2,21 +2,21 @@ var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
 var text = "";
 var i;
 for (i = 0; i < cars.length; i++) {
-    text += cars[i] + "<br>";
+  text += cars[i] + "<br>";
 }
 
 
 // The For/In Loop
 
 var person = {
-    fname: "John",
-    lname: "Doe",
-    age: 25
+  fname: "John",
+  lname: "Doe",
+  age: 25
 };
 var txt = "";
 var x;
 for (x in person) {
-    txt += person[x] + " ";
+  txt += person[x] + " ";
 }
 console.log(txt);
 
@@ -25,7 +25,7 @@ var txt = "";
 var numbers = [45, 4, 9, 16, 25];
 var x;
 for (x in numbers) {
-  txt += numbers[x] + "<br>"; 
+  txt += numbers[x] + "<br>";
 }
 console.log(txt);
 
@@ -36,19 +36,68 @@ var txt = "";
 var numbers = [45, 4, 9, 16, 25];
 numbers.forEach(myFunction);
 console.log(txt);
+
 function myFunction(value) {
-    txt = txt + value + "<br>"; 
-  }
+  txt = txt + value + "<br>";
+}
 
 
 //   --------------------------
 
 var cars = ["BMW", "Volvo", "Mini"];
 for (var x of cars) {
-    console.log(x)
-  }
+  console.log(x)
+}
 
-  var cars = ["BMW", "Volvo", "Mini"];
+var cars = ["BMW", "Volvo", "Mini"];
 for (var x of cars) {
-    console.log(x)
-  }
+  console.log(x)
+}
+
+// Statement 1
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var len = cars.length
+var text = "";
+var i = 0;
+for (; i < len; i++) {
+  text += cars[i] + " ";
+}
+console.log(text);
+
+// Statement 2
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var len = cars.length
+var text = "";
+var i = 2;
+for (; i < len; i++) {
+  text += cars[i] + " ";
+}
+console.log(text);
+
+
+// --------------------------------
+// The For/In Loop
+
+// The In Loop
+var tat = " ";
+var person = {
+  fname: "John",
+  lname: "Doe",
+  age: 25
+};
+var x;
+for (x in person) {
+  tat += person[x] + " ";
+}
+console.log(tat);
+
+// The in Loop
+var numbers = [45, 4, 9, 16, 25];
+var txt = "";
+var x;
+for (x in numbers) {
+  txt += numbers[x] + " ";
+}
+console.log(txt);
+
+
