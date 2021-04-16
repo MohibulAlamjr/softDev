72ae33c18a33fb97e24cbad4f5266c822b17aee3
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-17 00:37:41 +0600

    HtmlBootstrap

---
+++ sweety/javaScript/Java 1st/asset/js/main.js
@@ -0,0 +1,13 @@
+alert('Hlw,I am Sweet');
+
+function myFunction(){
+    document.getElementById("demo5").innerHTML = "5+6";   
+}
+console.log(2+3+8);
+
+var a=12;
+for(let i=2;i<=12;i++){
+    
+    console.log(a+i);
+}
+window.alert('Print this');
