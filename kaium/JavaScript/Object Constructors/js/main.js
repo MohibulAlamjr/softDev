d18885d279ff0274e419d3eb040021073f88cf1c
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-18 01:38:24 +0600

    object

--- kaium/JavaScript/Object Constructors/js/main.js
+++ kaium/JavaScript/Object Constructors/js/main.js
@@ -45,4 +45,73 @@ function Person(first, last, age, eye) {
  }
  console.log(kaium.bgroup);
  console.log(kaium.name());
- 
+
+
+//  Adding a Property to a Constructor
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  // You can NOT add a new property to a constructor function
+  Person.nationality = "English";
+
+  
+  // Create 2 Person objects
+  var myFather = new Person("John", "Doe", 50, "blue");
+  var myMother = new Person("Sally", "Rally", 48, "green");
+
+  console.log("The nationality of my father is " + myFather.nationality);
+
+//   Adding a Method to a Constructor
+
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+    this.name = function() {
+      return this.firstName + " " + this.lastName+" "+this.age
+    };
+  }
+  var myFather = new Person("Siddik", "Sorif", 50, "blue");
+  console.log("My father is " + myFather.name());
+
+
+//   The changeName() function
+  function Person3(firstName,lastName,age,eyeColor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.eyeColor = eyeColor;
+    this.changeName = function (name) {
+      this.lastName = name;
+    }
+  }
+  // Create a Person object
+  var myMother = new Person3("Sally","Begu",48,"green");
+  
+  // Change last name
+  myMother.changeName("Doe");
+  console.log("My mother's last name is " + myMother.lastName);
+
+//   Built-in JavaScript Constructors
+
+// var x1 = new Object();   // A new Object object
+// var x2 = new String();   // A new String object
+// var x3 = new Number();   // A new Number object
+// var x4 = new Boolean();  // A new Boolean object
+// var x5 = new Array();  // A new Array object
+// var x6 = new RegExp();   // A new RegExp object
+// var x7 = new Function(); // A new Function object
+// var x8 = new Date(); 
+
+// console.log("x1: " + typeof x1 + " " +
+// "x2: " + typeof x2 + " " +
+// "x3: " + typeof x3 + " " +
+// "x4: " + typeof x4 + " " +
+// "x5: " + typeof x5 + " " +
+// "x6: " + typeof x6 + " " +
+// "x7: " + typeof x7 + " " +
+// "x8: " + typeof x8 + " ";)
