655fb480b3ef4f25c0cdb717351040a463df393c
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-19 02:05:47 +0600

    aaa

---
+++ kaium/JavaScript/JS HTML DOM/DOM Elements/js/main.js
@@ -0,0 +1,13 @@
+// Finding HTML Element by Id
+
+
+// var myElement = document.getElementById("intro");
+// document.getElementById("demo").innerHTML = 
+// "The text from the intro paragraph is " + myElement.innerHTML;
+
+
+// Finding HTML Elements by Tag Name
+var x = document.getElementsByTagName("p");
+document.getElementById("demo").innerHTML = 
+'The text in first paragraph (index 0) is: ' + x[0].innerHTML;
+
