7501cd37cbef11bd913581050ae2fa8cd015a236
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-18 15:33:41 +0600

    Classes

--- kaium/JavaScript/js Classes 2/Class intro/js/main.js
+++ kaium/JavaScript/js Classes 2/Class intro/js/main.js
@@ -7,4 +7,30 @@ class car {
 
 }
 KaiumCar = new car("BMW",2021);
+Sweettea = new car("pelsur", 2020)
 console.log(KaiumCar.name + " " + KaiumCar.year);
+console.log(Sweettea.name + " " + Sweettea.year);
+
+// The Constructor Method
+class Car {
+    constructor(n, yr) {
+      this.name = n;
+      this.year = yr;
+  
+    }
+    age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    }
+    age2(x){
+        return x - this.year;
+    }
+  }
+  
+   myCar = new Car("Ford", 2014);
+//   let myCar = new Car("Ford", 2014);
+  console.log("My car is " + myCar.age() + " years old.");
+  let date = new Date();
+   y = date.getFullYear();
+  console.log(myCar.age2(y));
+
