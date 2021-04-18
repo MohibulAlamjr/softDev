11582a8a2898cd25a5166c212b7efefb9274afde
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-18 14:19:16 +0600

    Classes

---
+++ kaium/JavaScript/js Classes 2/Class intro/js/main.js
@@ -0,0 +1,10 @@
+// Classes
+class car {
+    constructor(Name,Year){
+        this.name = Name;
+        this.year = Year;
+    }
+
+}
+KaiumCar = new car("BMW",2021);
+console.log(KaiumCar.name + " " + KaiumCar.year);
