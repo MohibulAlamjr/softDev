dd93701a07d6c463af8789c5f2b6a640ead57cbc
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-20 02:23:15 +0600

    Dom CSS

---
+++ sweety/javaScript/DOM/DOM CSS/asset/js/main.js
@@ -0,0 +1,17 @@
+// CSS 
+
+function myFunction(){
+    document.getElementById("hello").style.color="red";
+    document.getElementById("hello1").style.color="red";
+    document.getElementById("hello2").style.color="Blue";
+    document.getElementById("hello3").style.color="gray";
+    document.getElementById("hello4").style.color="green";
+    document.getElementById("hello5").style.color="orange";
+    document.getElementById("hello6").style.color="Black";
+
+}
+
+var a = document.getElementsByClassName("h1");
+
+a[1].style.color="green";
+
