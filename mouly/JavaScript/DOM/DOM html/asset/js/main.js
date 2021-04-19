4767587c41ddf2578f0e52d3526c85b00211e0e8
Muniramouly <muniramouly25@gmail.com>
2021-04-19 20:04:21 +0600

    DOM HTML

---
+++ mouly/JavaScript/DOM/DOM html/asset/js/main.js
@@ -0,0 +1,29 @@
+ // changing html content 
+
+ document.getElementById("demo").innerHTML = " This is Rose";
+ document.getElementById("demo1").innerHTML = " This is Beli";
+
+
+ //changing html content using on click function
+
+
+ function myFun(){
+    document.getElementById("demo2").innerHTML = "I'am a student of Stamford University;"
+
+ }
+ 
+function myFunction (){
+    document.getElementById("demo3").innerHTML = "From shailkupa";
+}
+ 
+
+
+// changing the value of an attribute
+
+function myfun(){
+    document.getElementById("flower").src="asset/image/index.jpg";
+}
+
+function myfunction(){
+    document.getElementById("flower1").src="asset/image/images2.jpg";
+}
