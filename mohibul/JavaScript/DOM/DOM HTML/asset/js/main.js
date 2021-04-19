732b8879a03ee226b589f02d57398fcef7a375a9
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-19 15:56:02 +0600

    animation

---
+++ mohibul/JavaScript/DOM/DOM HTML/asset/js/main.js
@@ -0,0 +1,8 @@
+
+
+function myFun(){
+    // change element 
+ document.getElementById("demo").innerHTML="This is comes from Javascript page";
+//  change attribute
+ document.getElementById("image").src="asset/image/image2.jpeg";
+}
