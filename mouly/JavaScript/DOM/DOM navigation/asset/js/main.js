d135204287a284ee39f6a3498a37ebd005d24bb7
Muniramouly <muniramouly25@gmail.com>
2021-04-20 20:24:52 +0600

    Navigation

---
+++ mouly/JavaScript/DOM/DOM navigation/asset/js/main.js
@@ -0,0 +1,12 @@
+// Child Nodes and Node Values
+
+
+document.getElementById("id02").innerHTML = document.getElementById("id01").innerHTML;
+
+
+document.getElementById("id022").innerHTML = document.getElementById("id011").innerHTML;
+
+document.getElementById("id023").innerHTML = document.getElementById("id013").firstChild.nodeValue;
+
+
+document.getElementById("id024").innerHTML = document.getElementById("id014").childNodes[0].nodeValue;
