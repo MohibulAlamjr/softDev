111f4b97ca322280bd1cd71d6df6ada527af7530
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-20 14:49:53 +0600

    event

---
+++ sweety/javaScript/DOM/DOM Events/asset/js/main.js
@@ -0,0 +1,51 @@
+
+// Change Text 
+
+function changeText(id) {
+    id.innerHTML = "Alhamdulillah";
+  }
+
+
+  document.getElementById("bttn").onclick = myfun;
+
+ function myfun(){
+     document.getElementById("hi").innerHTML = Date();
+ }
+
+
+//  on Mousover and out 
+
+function Cin(b) {
+    b.innerHTML = "Hello";
+  }
+  
+  function Cout(c) {
+    c.innerHTML = "World!";
+  }
+
+
+  function cc(a) {
+    a.innerHTML = "Hey";
+  }
+  
+  function cd(d) {
+    d.innerHTML = "softDevians";
+  }
+
+
+  function ab(x){
+      x.innerHTML = "Click";
+  }
+
+  function ac(y){
+      y.innerHTML = "Here";
+  }
+
+
+  function abc(i){
+    i.innerHTML = "Game over";
+}
+
+function acd(j){
+    j.innerHTML = "YAEH!";
+}
