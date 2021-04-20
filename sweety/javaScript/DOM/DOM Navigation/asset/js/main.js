116e6e8626dab4839ab458494dd1aa6e35bbfb12
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-21 02:31:27 +0600

    Navigation

---
+++ sweety/javaScript/DOM/DOM Navigation/asset/js/main.js
@@ -0,0 +1,24 @@
+
+// Child Nodes and Node Values
+
+
+document.getElementById("hi1").innerHTML = document.getElementById("hi").innerHTML;
+
+
+document.getElementById("hi3").innerHTML = document.getElementById("hi2").innerHTML;
+
+
+document.getElementById("hi5").innerHTML = document.getElementById("hi4").innerHTML;
+
+
+document.getElementById("hi7").innerHTML = document.getElementById("hi6").innerHTML;
+
+document.getElementById("hi9").innerHTML = document.getElementById("hi8").innerHTML;
+
+
+document.getElementById("id1").innerHTML = document.getElementById("id").innerHTML;
+
+
+document.getElementById("id3").innerHTML = document.getElementById("id2").innerHTML;
+
+
