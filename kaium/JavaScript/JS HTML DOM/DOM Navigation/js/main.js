e799678a95040989813955ecae9c2809b01f1a9d
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-21 16:54:33 +0600

    aa

--- kaium/JavaScript/JS HTML DOM/DOM Navigation/js/main.js
+++ kaium/JavaScript/JS HTML DOM/DOM Navigation/js/main.js
@@ -1,6 +1,6 @@
 document.getElementById("id02").innerHTML = document.getElementById("test").innerHTML;
 
-document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
+document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[1].nodeValue;
 
 document.getElementById("id01").innerHTML = document.getElementById("id01").nodeName;
 
