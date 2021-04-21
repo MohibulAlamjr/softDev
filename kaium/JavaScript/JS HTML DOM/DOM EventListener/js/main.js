b99e32e7e8a358e17aca2769646bc8e86de2c9d1
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-21 16:59:47 +0600

    aaa

--- kaium/JavaScript/JS HTML DOM/DOM EventListener/js/main.js
+++ kaium/JavaScript/JS HTML DOM/DOM EventListener/js/main.js
@@ -14,8 +14,12 @@ document.getElementById("myBtn").addEventListener("click", function() {
          document.getElementById("Demo").innerHTML = "mouly";
          
      }
+
+        var x = document.getElementById("btn2");
+    x.addEventListener("click",Myfun3) ;
+     x.addEventListener("click",Myfun4);
      function Myfun2() {
-         document.getElementById("Demo2").innerHTML = "Kaium";
+         document.getElementById("Demo4").innerHTML = "Kaium";
          
      }
 
