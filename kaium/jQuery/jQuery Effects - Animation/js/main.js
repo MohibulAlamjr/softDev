175a6e9cf302078eb9b34050787b56fb86895a9b
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-22 01:01:49 +0600

    aser

--- kaium/jQuery/jQuery Effects - Animation/js/main.js
+++ kaium/jQuery/jQuery Effects - Animation/js/main.js
@@ -1,7 +1,18 @@
 // jQuery animate() - Manipulate Multiple Properties
 $(document).ready(function(){
-  $("button").click(function(){
+  $("#btn").click(function(){
     $("div").animate({left: '250px'});
   });
 });
 
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("div").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
+    });
+  });
+});
