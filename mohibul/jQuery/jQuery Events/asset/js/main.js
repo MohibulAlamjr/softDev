40ad12a30c0df0b7ed7fab49878da9a171c38f69
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-21 11:23:30 +0600

    jquary

---
+++ mohibul/jQuery/jQuery Events/asset/js/main.js
@@ -0,0 +1,68 @@
+// on 
+
+$(document).ready(function(){
+ $("p.p02").on("click",function(){
+     $(this).hide();
+ })
+
+})
+// double click event 
+$(document).ready(function(){
+    $("p.p03").dblclick(function(){
+        $(this).hide();
+    })
+   
+   })
+//    mouseente
+   $(document).ready(function(){
+    $("#p04").mouseenter(function(){
+        $("#pp").hide();
+        $("#pp1").show();
+    })
+   
+   })
+
+//    mouseleave
+   $(document).ready(function(){
+    $("#p05").mouseleave(function(){
+        $("#pp1").hide();
+        $("#pp").show();
+        
+    })
+   
+   })
+
+   <!-- focus -->
+
+   $(document).ready(function(){
+    $("input").focus(function(){
+        $(this).css("background-color", "yellow");
+        
+ 
+    })
+    $("input").blur(function(){
+        $(this).css("background-color", "blue");
+        
+ 
+    })
+
+   
+   })
+
+//    on 
+
+$(document).ready(function(){
+    $("#onid").on({
+
+        mouseenter: function(){
+            $(this).css("background-color", "lightgray");
+        },
+        mouseleave: function(){
+            $(this).css("background-color", "blue");
+        },
+        click: function(){
+            $(this).css("background-color", "red");  
+        }
+    })
+})
+
