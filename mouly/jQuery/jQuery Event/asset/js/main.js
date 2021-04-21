a2d9e8e9eda6b6927f240bd6d3373fbe5a295ba7
Muniramouly <muniramouly25@gmail.com>
2021-04-21 21:23:17 +0600

    Selectors

---
+++ mouly/jQuery/jQuery Event/asset/js/main.js
@@ -0,0 +1,41 @@
+// one click 
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+
+
+// double click 
+
+
+$(document).ready(function(){
+  $(".flower").dblclick(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("p.p03").dblclick(function(){
+      $(this).hide();
+  })
+ 
+ })
+
+
+
+
+$(document).ready(function(){
+    $("h1").on({
+      mouseenter: function(){
+        $(this).css("background-color", "lightgray");
+      },  
+      mouseleave: function(){
+        $(this).css("background-color", "lightblue");
+      }, 
+      click: function(){
+        $(this).css("background-color", "yellow");
+      }  
+    });
+  });
