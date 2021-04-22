9030da004d2889d85e2f41ff924fb94539c79a86
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-22 18:20:12 +0600

    toggle

--- kaium/jQuery/jQuery Effects/js/main.js
+++ kaium/jQuery/jQuery Effects/js/main.js
@@ -1,8 +1,19 @@
+
+// jQuery hide() and show()
+
 $(document).ready(function(){
   $("#hide").click(function(){
-    $("p").hide();
+    $("p").hide(1000);
   });
   $("#show").click(function(){
-    $("p").show();
+    $("p").show(2000);
+  });
+});
+
+// jQuery toggle()
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("#p1").toggle(1000);
+     $("#p2").toggle(1000);
   });
 });
