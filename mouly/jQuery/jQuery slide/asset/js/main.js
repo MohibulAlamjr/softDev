8f496a910e2615af2e643dacfe774acf924c9b8f
Muniramouly <muniramouly25@gmail.com>
2021-04-22 12:55:53 +0600

    toggole

---
+++ mouly/jQuery/jQuery slide/asset/js/main.js
@@ -0,0 +1,83 @@
+// slidedown 
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  });
+
+
+  $(document).ready(function(){
+      $("#div1").click(function(){
+          $("#div2").slideDown(1000);
+      });
+  });
+
+
+//   slideup 
+
+$(document).ready(function(){
+    $("#div3").click(function(){
+        $("#div4").slideUp(2000);
+    });
+});
+
+
+$(document).ready(function(){
+    $("#div5").click(function(){
+        $("#div6").slideUp("slow");
+    });
+});
+
+
+// slide toggole 
+
+$(document).ready(function(){
+    $("#div7").click(function(){
+        $("#div8").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#div9").click(function(){
+        $("#div10").slideToggle(2000);
+    });
+});
+
+
+// slide up and slide down 
+
+$(document).ready(function(){
+    $("#div11").click(function(){
+        $("#div12").slideUp(3000);
+        $("#div13").slideDown(1000);
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $("#div111").click(function(){
+        $("#div121").slideDown(3000);
+        $("#div131").slideUp(1000);
+    });
+});
+
+
+// slide up and toggole 
+
+$(document).ready(function(){
+    $("#div011").click(function(){
+        $("#div021").slideToggle(3000);
+        $("#div031").slideUp(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $("#div0011").click(function(){
+        $("#div0021").slideUp(3000);
+        $("#div0031").slideToggle(1000);
+    });
+});
