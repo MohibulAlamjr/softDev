ea5eda8d7eb71ff1d8e98f43b392c022e06dcac4
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-22 18:43:20 +0600

    fadeout

--- kaium/jQuery/jQuery Effects/js/main.js
+++ kaium/jQuery/jQuery Effects/js/main.js
@@ -20,3 +20,21 @@ $(document).ready(function(){
 
 // jQuery Effects - Fading
 
+// jQuery fadeIn() Method
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+  });
+});
+
+
+// jQuery fadeOut() Method
+$(document).ready(function(){
+  $("#btn3").click(function(){
+    $("#div4").fadeOut();
+    $("#div5").fadeOut("slow");
+    $("#div6").fadeOut(3000);
+  });
+});
