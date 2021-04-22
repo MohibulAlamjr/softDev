5b6a7ff543511b3282d54dd255bcc1c7ab79c4c3
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-22 17:11:24 +0600

    Slide

--- sweety/JQuery/JQ Effects/03.JQ Slide/asset/js/main.js
+++ sweety/JQuery/JQ Effects/03.JQ Slide/asset/js/main.js
@@ -1,3 +1,24 @@
+
+// Slide Down 
+
+$(document).ready(function(){
+    $("#id2").click(function(){
+      $("#id1").slideDown("slow");
+    });
+  });
+
+//   Slide Up 
+
+
+$(document).ready(function(){
+    $("#id4").click(function(){
+      $("#id3").slideUp("slow");
+    });
+  });
+
+
+//   Another One 
+
 $(document).ready(function(){
     $("#d").click(function(){
         $("#dd").slideDown("slow");
