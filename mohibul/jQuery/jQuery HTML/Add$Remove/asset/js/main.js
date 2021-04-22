66cf9fa1fd4b5f999914b84dae23993d9206dabd
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-22 23:41:50 +0600

    append

--- mohibul/jQuery/jQuery HTML/Add$Remove/asset/js/main.js
+++ mohibul/jQuery/jQuery HTML/Add$Remove/asset/js/main.js
@@ -1,46 +1,9 @@
-// text 
-$(document).ready(function(){
-  $("#btn1").click(function(){
-      var x= $("#p01").text();
-      $("#h01").text(x);
-   })
 
+// append
+$(document).ready(function(){
+  $("#div01").append("<p>hi, Mouly</p>");
 })
-// html 
+// prepend
 $(document).ready(function(){
-    $("#btn2").click(function(){
-        var y= $("#p01").html();
-        $("#h02").html(y);
-        
-     })
-  
-  })
-// val 
-  $(document).ready(function(){
-    $("#btn3").click(function(){
-        var z= $("#in01").val();
-        $("#h03").text(z);
-        
-     })
-  
-  })
-
-//   src 
-  $(document).ready(function(){
-    $("#btn4").click(function(){
-        var m= $("#image").attr("src");
-        $("#img2").attr("src",m);
-        
-     })
-  
-  })
-
-// href 
-  $(document).ready(function(){
-    $("#btn5").click(function(){
-        var n= $("#ancer").attr("href");
-        $("#ancer2").attr("href",n);
-        
-     })
-  
-  })
+  $("#div02").prepend("<p>hi, Mouly</p>");
+})
