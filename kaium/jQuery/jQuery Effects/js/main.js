f1dfe71cceb8fe584f7abff241d13df09973e673
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-22 18:15:41 +0600

    JQ

--- kaium/jQuery/jQuery Effects/js/main.js
+++ kaium/jQuery/jQuery Effects/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-    $("#hide").click(function () {
-        $("p").hide();
-    });
-    $("#show").click(function () {
-        $("p").show();
-    });
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
 });
