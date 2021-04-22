6c8dc3b63c3a497286d0f7ccddc3f93f88207253
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-22 23:13:22 +0600

    aa

---
+++ kaium/jQuery/jQuery Get Set Add Remove/js/main.js
@@ -0,0 +1,18 @@
+// Get Content - text(), html(), and val()
+
+$(document).ready(function () {
+    $("#btn1").click(function () {
+        alert("Text: " + $("#test").text());
+    });
+    $("#btn2").click(function () {
+        alert("HTML: " + $("#test").html());
+    });
+})
+
+
+$(document).ready(function(){
+    $("").click(function(){
+        var z = $("").html
+    })
+})
+
