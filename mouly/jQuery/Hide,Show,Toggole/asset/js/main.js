7f675a83193da910c9f1bac4b3162d33608da455
Muniramouly <muniramouly25@gmail.com>
2021-04-22 09:17:06 +0600

    Hide show toggle

---
+++ mouly/jQuery/Hide,Show,Toggole/asset/js/main.js
@@ -0,0 +1,85 @@
+// hide 
+$(document).ready(function(){
+    $("#btn").click (function(){
+        $("#hide").hide();
+    });
+    
+});
+
+
+
+$(document).ready (function(){
+
+    $("#btn1").click(function(){
+        $("#good").hide(1000);
+    });
+
+});
+
+
+$(document).ready (function(){
+
+    $("#btn2").click(function(){
+        $("#noon").hide();
+    });
+
+});
+
+
+$(document).ready (function(){
+
+    $("#btn11").click(function(){
+        $("#para").hide(1000);
+    });
+
+});
+
+// show 
+
+
+$(document).ready(function(){
+    $("#btn3").click (function(){
+        $("#hide1").show();
+    });
+    
+});
+
+
+
+$(document).ready (function(){
+
+    $("#btn4").click(function(){
+        $("#good1").show(1000);
+    });
+
+});
+
+
+$(document).ready (function(){
+
+    $("#btn5").click(function(){
+        $("#noon1").show();
+    });
+
+});
+
+
+$(document).ready (function(){
+
+    $("#btn6").click(function(){
+        $("#para1").show(1000);
+    });
+
+});
+
+
+// Toggle 
+
+
+$(document).ready (function(){
+
+    $("#btn7").click(function(){
+        $("#nam").toggle(1000);
+    });
+
+});
