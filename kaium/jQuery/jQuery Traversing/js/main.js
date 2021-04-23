22d0c756ab416ce8327b2784b41582a3d7d56acd
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-23 15:50:52 +0600

    aser

--- kaium/jQuery/jQuery Traversing/js/main.js
+++ kaium/jQuery/jQuery Traversing/js/main.js
@@ -17,3 +17,17 @@ $(document).ready(function(){
 
 
 })
+
+// $(document).ready(function(){
+//     $("span").parentsUntil('html').addClass('fparent4')
+
+
+// })
+
+// jQuery children() Method
+
+$(document).ready(function(){
+    $("div").find('s01').addClass('fparent5')
+
+
+})
