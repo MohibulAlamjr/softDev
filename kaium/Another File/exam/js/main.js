7b6b4f0df02bb0d158799b819860ba62e21f9b08
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-25 01:44:30 +0600

    kaium

---
+++ kaium/Another File/exam/js/main.js
@@ -0,0 +1,24 @@
+// let a;
+// let b=a;
+// a=90
+// console.log(b);
+
+// int x = 8;
+// if(x>9){
+//     document.write(9);
+// }
+// else{
+//     document.write(x);
+// }
+// console.log(x);
+// a= 67;
+// a= 56;
+// console.log(a);
+// let a = {
+    
+// };
+// console.log(a.name)
+
+var fruits = ["Banana", "Orange", "Apple"];
+fruits.unshift("Jackfruit, Lemon");
+console.log(fruits);
