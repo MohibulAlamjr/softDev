8570841958aff217813bf8c31f00169e32e1a95c
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-24 20:05:41 +0600

    Adding Removing

--- sweety/JQuery/JQ HTML/02.Add & Remove/asset/js/main.js
+++ sweety/JQuery/JQ HTML/02.Add & Remove/asset/js/main.js
@@ -58,14 +58,112 @@ $(document).ready(function(){
 
 
 
+$(document).ready(function(){
+  $("#jj1").click(function(){
+    $("#pp1").before("<b>Before</b>");
+  });
+
+  $("#jjj1").click(function(){
+    $("#pp1").after("<i>After</i>");
+  });
+});
 
 
 
+$(document).ready(function(){
+  $("#jj2").click(function(){
+    $("#pp2").before("<b>Before</b>");
+  });
+
+  $("#jjj2").click(function(){
+    $("#pp2").after("<i><b>After<b></i>");
+  });
+});
+
+
+// Remove Elements
+
+
+$(document).ready(function(){
+  $("#rmv").click(function(){
+    $("#div1").remove();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#rmv1").click(function(){
+    $("#div2").remove();
+  });
+});
 
 
 
+$(document).ready(function(){
+  $("#rmv2").click(function(){
+    $("#div3").remove();
+  });
+});
 
 
+$(document).ready(function(){
+  $("#rmv3").click(function(){
+    $("#div4").remove();
+  });
+});
 
+
+// empty() Method
+
+
+$(document).ready(function(){
+  $("#ooo").click(function(){
+    $("#divv").empty();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#ooo1").click(function(){
+    $("#divv1").empty();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#ooo2").click(function(){
+    $("#divv2").empty();
+  });
+});
+
+$(document).ready(function(){
+  $("#ooo3").click(function(){
+    $("#divv3").empty();
+  });
+});
+
+
+// Filter the Elements to be Removed
+
+
+$(document).ready(function(){
+  $("#kkk").click(function(){
+    $("p").remove(".test");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#kkkk").click(function(){
+    $(".test1").remove(".test1");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#09").click(function(){
+    $("p").remove(".tes, .demo");
+  });
+});
 
 
