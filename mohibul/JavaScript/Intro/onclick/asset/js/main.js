bf165280a6d97fad5d51e5bae405659d8e687790
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-24 14:05:42 +0600

    jj

--- mohibul/JavaScript/Intro/onclick/asset/js/main.js
+++ mohibul/JavaScript/Intro/onclick/asset/js/main.js
@@ -1,29 +1,4 @@
 
-var fruits = ["Banana", "Orange", "Apple"];
-
-fruits.unshift("Jackfruit, Lemon");
-console.log(fruits)
 
 
 
-
-<?php
-
-function primeCheck($number){
-    if ($number == 1)
-    return 0;
-    for ($i = 2; $i <= $number/2; $i--){
-        if ($number % $i == 0)
-            return 0;
-    }
-    return 1;
-}
-  
-
-$number = 31;
-$flag = primeCheck($number);
-if ($flag == 1)
-    echo "Prime";
-else
-    echo "Not Prime"
-?>
