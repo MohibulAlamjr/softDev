e3d342c58cd0986d064c5b7e74c60aa9cf984383
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-25 23:24:13 +0600

    not to do

--- kaium/project/asset/js/main.js
+++ kaium/project/asset/js/main.js
@@ -1,92 +1,77 @@
-$(document).ready(function(){
-    $("#btn1").click(function(){
-       $("#Number").hide();
-       $("#oneNumber").show(); 
-    })
-
-    $("#btn2").click(function(){
+$(document).ready(function () {
+    $("#btn1").click(function () {
         $("#Number").hide();
-        $("#lenghtNumber").show(); 
-     })
-
-     $(".res").click(function(){
+        $("#oneNumber").show();
+    })
+    $(".res").click(function () {
         $(".common").hide();
-        $("#result").show(); 
-     })
+        $("#result").show();
+    })
 })
 
 // Celcius to Fahrenheit
 
-$(document).ready(function(){
-    $("#btn3").click(function(){
-     var number = $("#input1").val();
-     function singlePrime(x){
+$(document).ready(function () {
+    $("#btn3").click(function () {
+        var number = $("#input1").val();
+
+        function singlePrime(x) {
 
 
-         number=(x-32)/1.8;
-        //  number=(x*1.8)+32;
-     }
+            //  number=(x-32)/1.8;
+            number = (x * 1.8) + 32;
+        }
 
-     let result = singlePrime(number);
-   if(result === 1){
-   let y = number + " is a Fahrenheit";
-    $("#result").show();
-    $("#pResult").text(y);
-   }else{
-    let z = number + " is not a Fahrenheit";
-    $("#result").show();
-    $("#pResult").text(z);
-   }
-
-   
-
+        let result = singlePrime(number);
+        if (result === 1) {
+            let y = number + " is a Fahrenheit";
+            $("#result").show();
+            $("#pResult").text(y);
+        } else {
+            let z = number + " is not a Fahrenheit";
+            $("#result").show();
+            $("#pResult").text(z);
+        }
     })
 
 })
 
 
 
-$(document).ready(function(){
-    $("#btn2").click(function(){
-       $("#Number").hide();
-       $("#oneNumber").show(); 
-    })
-
-    $("#btn1").click(function(){
+$(document).ready(function () {
+    
+    $("#btn2").click(function () {
         $("#Number").hide();
-        $("#lenghtNumber").show(); 
-     })
+        $("#lenghtNumber").show();
+    })
 
-     $(".res").click(function(){
+    $(".res").click(function () {
         $(".common").hide();
-        $("#result").show(); 
-     })
+        $("#result").show();
+    })
 })
 
-// Celcius to Fahrenheit
+//Fahrenheit to Celcius  
 
-$(document).ready(function(){
-    $("#btn3").click(function(){
-     var number = $("#input1").val();
-     function singlePrime(x){
+$(document).ready(function () {
+    $("#btn3").click(function () {
+        var number = $("#input1").val();
+
+        function singlePrime(x) {
 
 
-         number=(x-32)/1.8;
-        //  number=(x*1.8)+32;
-     }
+            number = (x - 32) / 1.8;
+            //  number=(x*1.8)+32;
+        }
 
-     let result = singlePrime(number);
-   if(result === 1){
-   let y = number + " is a Celcius";
-    $("#result").show();
-    $("#pResult").text(y);
-   }else{
-    let z = number + " is not a Celcius";
-    $("#result").show();
-    $("#pResult").text(z);
-   }
+        let result = singlePrime(number);
+        if (result === 1) {
+            let z = number + " is  a Celcius";
+            $("#result").show();
+            $("#pResult").text(z);
+        }
 
-   
+
 
     })
 
