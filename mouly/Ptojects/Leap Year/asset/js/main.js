f87a0f7562db2d340aa88fa46fc95336d841f038
Muniramouly <muniramouly25@gmail.com>
2021-04-25 21:05:58 +0600

    gg

--- mouly/Ptojects/Leap Year/asset/js/main.js
+++ mouly/Ptojects/Leap Year/asset/js/main.js
@@ -35,3 +35,35 @@ $(document).ready(function(){
     })
 
 });
+
+
+
+// calculation 
+
+$(document).ready(function(){
+    $("bt3").click(function(){
+       var leap = $("#input").val();
+        function leapYear (year){
+          
+            
+             for(let i = 0; i = year; i ++){
+
+             var l = 0;
+             if(year % 400 ==0){
+               
+
+             }
+             else if(year % 1001 == 0){
+                
+               
+             }
+             else if(year % 4 == 0){
+               
+             }
+            }
+
+
+
+        
+    });
+});
