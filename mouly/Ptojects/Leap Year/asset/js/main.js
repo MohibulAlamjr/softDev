033766ee61450c194aff4fd6806fa72eae41a49a
Muniramouly <muniramouly25@gmail.com>
2021-04-25 10:58:45 +0600

    aaaa

---
+++ mouly/Ptojects/Leap Year/asset/js/main.js
@@ -0,0 +1,37 @@
+// for button 
+
+$(document).ready(function(){
+    $("#bt1").click(function(){
+       $("#year").hide();
+       $("#oneYear").show(); 
+    })
+
+});
+
+
+$(document).ready(function(){
+    $("#bt3").click(function(){
+        $("#oneYear").hide(); 
+      $("#output1").show(); 
+    })
+
+});
+
+
+
+$(document).ready(function(){
+    $("#bt2").click(function(){
+       $("#year").hide();
+       $("#year2").show(); 
+    })
+
+});
+
+
+$(document).ready(function(){
+    $("#bt4").click(function(){
+        $("#year2").hide(); 
+      $("#output1").show(); 
+    })
+
+});
