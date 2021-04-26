0a898df42f4c6d042ddec6886fd77607c3ad9693
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-04-26 16:25:33 +0600

    module

--- sweety/Node.JS/HTTP Module/demo_querystring.js
+++ sweety/Node.JS/HTTP Module/demo_querystring.js
@@ -4,6 +4,6 @@ var url = require('url');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
-  var txt = q.id + " " + q.pass;
+  var txt = q.year + " " + q.month;
   res.end(txt);
 }).listen(8080);
