3f2d6e012f9d1be270d22f0ae1bed174871fa1ea
Mohibul Alam <mohibulalamjr@gmail.com>
2021-04-30 00:46:07 +0600

    insert

---
+++ mohibul/Node.js/mysql/insert/index.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "softDev"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO `admin`(`name`, `bloodgroup`, `email`, `password`) VALUES ('Mohibul Alam','AB(+)','mohibulalamjr@gmail.com','mohibul123')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
