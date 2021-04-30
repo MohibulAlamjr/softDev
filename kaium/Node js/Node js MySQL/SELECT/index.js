7fdc02125dae609eb0027081e6fa86b61cf6c862
kaiumgithub <kaium.github.cse@gmail.com>
2021-04-30 12:06:55 +0600

    select

---
+++ kaium/Node js/Node js MySQL/SELECT/index.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "softdev"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "SELECT `id`, `name`, `bloodgroup`FROM `admin` WHERE id='1'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
