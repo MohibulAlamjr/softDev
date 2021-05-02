981c7fbca89d0b032197cf273086774f73994bd2
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-02 16:09:05 +0600

    aaqaa

--- kaium/Node js/Node2/URLModule/index.js
+++ kaium/Node js/Node2/URLModule/index.js
@@ -1,5 +1,5 @@
-const { writeSync } = require('fs');
 var http =require('http');
+var URL= require('url');
 var server = http.createServer(function(req,res){
 
 var myURL ="http://kaium.com/blog.html?year=2021&month=july";
@@ -9,11 +9,11 @@ var mypathName = myURLobj.pathname;
 var mySearchName = myURLobj.search;
 
 res.writeHead(200,{'Content-Type':'text/html'})
-res.write(myhostName);
+res.write(mypathName);
 res.end();
 
 
  
 });
-server.listen(1080);
+server.listen(8080);
 console.log("Server Run Success");
