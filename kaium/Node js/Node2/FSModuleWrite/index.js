dddf6cb8cb2a1e707b4243f2527af37ce5348875
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-02 17:12:33 +0600

    ase

--- kaium/Node js/Node2/FSModuleWrite/index.js
+++ kaium/Node js/Node2/FSModuleWrite/index.js
@@ -2,19 +2,20 @@ var fs = require('fs');
 var http = require('http');
 
 
-var server = http.createServer(function(req,res){
+var server = http.createServer(function (req, res) {
 
-if (req.url=="/") {
-fs.readFile('index.html',function(error,data){
-
-    res.writeHead(200,{'Content-Type':'text/html '});
-    res.write(data);
-    res.end();
-
-});
+    if (req.url == "/") {
     
-}
+
+        let mydata = fs.readFileSync('index.html');
+        res.writeHead(200, {
+            'Content-Type': 'text/html '
+        });
+        res.write(mydata);
+        res.end();
+
+    }
 
 });
-server.listen(2020);
+server.listen(2060);
 console.log("Server Run Success");
