b1597dd47a041dcf003a3234742d0f97d9f2156a
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-02 15:45:42 +0600

    aaa

--- kaium/Node js/Node2/URLModule/main.js
+++ kaium/Node js/Node2/URLModule/main.js
@@ -1,12 +1,7 @@
 var http =require('http');
 var server = http.createServer(function(req,res){
 
-if (req.url=="/") {
-res.writeHead(200,{'content-type':'text/html'})
-res.write('<h1>this is Home Page</h1>')
-res.end();
-    
-}
+var myURL ="http://kaium.com/blog.html?year=2021&month=july";
 
  
 });
