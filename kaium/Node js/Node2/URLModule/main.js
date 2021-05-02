38f51cfb4db0c5bdeabe83fb4923ed8df062a21f
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-02 15:50:40 +0600

    asfd

--- kaium/Node js/Node2/URLModule/main.js
+++ kaium/Node js/Node2/URLModule/main.js
@@ -1,7 +1,14 @@
+const { writeSync } = require('fs');
 var http =require('http');
 var server = http.createServer(function(req,res){
 
 var myURL ="http://kaium.com/blog.html?year=2021&month=july";
+var myURLobj = URL.parse(myURL,true);
+var myhostName = myURLobj.host;
+var mypathName = myURLobj.pathname;
+var mySearchName = myURLobj.search;
+write(myhostName);
+
 
  
 });
