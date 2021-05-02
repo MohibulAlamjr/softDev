e888cc957d4fb3748cafa9583c3630736b91fc76
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-02 17:18:03 +0600

    last

--- kaium/Node js/Node2/FSModuleWrite/index.js
+++ kaium/Node js/Node2/FSModuleWrite/index.js
@@ -5,14 +5,29 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
 
     if (req.url == "/") {
-    
+
+        fs.writeFile('demo.txt', 'hello Kaium', function (error) {
+            if (error) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html '
+                });
+                res.write("file Write Fail");
+                res.end();
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html '
+                });
+                res.write("file Write Success");
+                res.end();
+
+
+            }
+
+        });
+
 
         let mydata = fs.readFileSync('index.html');
-        res.writeHead(200, {
-            'Content-Type': 'text/html '
-        });
-        res.write(mydata);
-        res.end();
+
 
     }
 
