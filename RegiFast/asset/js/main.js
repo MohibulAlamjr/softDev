3b8649e44183622b46d166ab1f827750ffd6bd76
Mohibul Alam <mohibulalamjr@gmail.com>
2021-05-04 02:07:48 +0600

    regifast'

--- RegiFast/asset/js/main.js
+++ RegiFast/asset/js/main.js
@@ -14,20 +14,24 @@
 
 
 
-$(document).ready(function(){
-    $("#btn02").click(function(){
-        var u = $("#inlineFormInput").val();
-        var v =$("#inlineFormInputGroup").val();
-        var id=$("#sid").text();
-        alert("Course Name="+ u + " and Course ID=" + v +"Successfully Included" );
+// $(document).ready(function(){
+//     $("#btn02").click(function(){
+//         var u = $("#inlineFormInput").val();
+//         var v =$("#inlineFormInputGroup").val();
+//         var id=$("#sid").text();
+//         alert("Course Name="+ u + " and Course ID=" + v +"Successfully Included" );
 
-        var xhttp = new XMLHttpRequest();
+//         var xhttp = new XMLHttpRequest();
       
-        xhttp.open("GET","try.php?q=" + u +'&u='+ v+'&t='+ id , true);
-        xhttp.send();
+//         xhttp.open("GET","try.php?q=" + u +'&u='+ v+'&t='+ id , true);
+//         xhttp.send();
+//     })
+// })
+
+$(document).ready(function(){
+    $("#btn001").click(function(){
+        $("#btn002").fadeIn(2000);
     })
 })
-
-
 
 
