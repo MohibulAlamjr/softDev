b4bdfde9a2b77b93d87f1f494a3c5748d5bccdef
kaiumgithub <kaium.github.cse@gmail.com>
2021-05-04 16:30:42 +0600

    npm

---
+++ kaium/Node js/MongoDB/Find.js
@@ -0,0 +1,42 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://weAresoftDev:x8rAY6ffshuejQk@cluster0.vapqc.mongodb.net?retryWrites=true&w=majority";
+var config = {
+    useUnifiedTopology: true
+}
+MongoClient.connect(url, config, function (error, myclicnt) {
+
+    if (error) {
+        console.log("failed");
+    } else {
+
+        console.log("done");
+        insert(myclicnt)
+        // deleteData(myclicnt)
+        // deleteallData(myclicnt)
+    }
+
+
+})
+
+function insert(connect) {
+
+    var mydatabase = connect.db("softDev");
+    var mycollection = mydatabase.collection("admin");
+    var person = {
+        name: " kaium",
+        position: "Core Member of softDev",
+        city: "Barguna",
+        phoneNumber: "01785956149",
+        serial: "08",
+        bloodgroup: "B(+)"
+    }
+
+    mycollection.insertOne(person, function (error) {
+        if (error) {
+            console.log("Data insert failed");
+        } else {
+            console.log("Data insert succecfully");
+        }
+    })
+
+}
