a43cc770e094123305550812499896a9814b1a3b
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-05-15 15:54:14 +0600

    fb

---
+++ sweety/Template/Jevelin Template/asset/js/main.js
@@ -0,0 +1,29 @@
+
+// Slide Troggle 
+
+$(document).ready(function(){
+    $("#p").click(function(){
+      $("#lorem1").toggle(200);
+    });
+  });
+
+// Second Troggle 
+
+$(document).ready(function(){
+  $("#Laboratory").click(function(){
+    $("#lorem2").slideToggle(200);
+  });
+});
+
+// Third Toggle 
+
+
+$(document).ready(function(){
+  $("#Diagnostic").click(function(){
+    $("#lorem3").slideToggle(200);
+  });
+});
+
+
+
+
