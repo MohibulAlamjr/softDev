9cf0bc63d33109d9e6e02563883490a476f9c8b3
Mohibul Alam <mohibulalamjr@gmail.com>
2021-05-19 00:28:11 +0600

    event

---
+++ mohibul/jQuery/softDevEvent/softDev.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("#hideEffect").hide(1000)
+    })
+})
+
+$(document).ready(function(){
+    $("#toggole").click(function(){
+        $("#toggoleEffect").toggle(1000)
+    })
+})
