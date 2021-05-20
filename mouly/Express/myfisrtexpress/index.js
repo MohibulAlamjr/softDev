57bb851d5ffa00a9b79caf658e059c26f16a84c9
Muniramouly <muniramouly25@gmail.com>
2021-05-20 10:36:13 +0600

    express

--- mouly/Express/myfisrtexpress/index.js
+++ mouly/Express/myfisrtexpress/index.js
@@ -81,33 +81,82 @@ const port = 8080;
 // ////// JSON Response ////////
 
     
-app.post('/three', function(req,res){
+// app.post('/three', function(req,res){
 
-    let myJSONArray = [
-        {
-        fname:"serajum",
-        city:"Jhenidah ",
-        age : 24
-        },
-        {
-            fname:"Era",
-            city:"shailkupa",
-            age :17
-        },
-        {
-            fname : "Ema",
-            city: "Foridpur",
-            age: 28
-        }
-    ]
+//     let myJSONArray = [
+//         {
+//         fname:"serajum",
+//         city:"Jhenidah ",
+//         age : 24
+//         },
+//         {
+//             fname:"Era",
+//             city:"shailkupa",
+//             age :17
+//         },
+//         {
+//             fname : "Ema",
+//             city: "Foridpur",
+//             age: 28
+//         }
+//     ]
 
 
     
-    res.json( myJSONArray);
+//     res.json( myJSONArray);
 
- })
+//  })
 
- 
+
+/////// upload files /////
+
+// app.get('/five', function(req,res){
+//          res.download("./upload/image/index.jpg");
+//       })
+
+
+      app.get("/Bangladesh", function(req,res){
+
+        res.redirect("http://localhost:" + port +"/Shyllet")
+
+      })
+
+      
+      app.get("/Shyllet", function(req,res){
+
+        res.send("This is beautiful")
+
+      })
+
+
+
+      app.get("/six", function(req,res){
+
+
+        res.append("name", "Serajum munira");
+        res.append("city", "Jhenidah");
+        res.append("age", "24 years old");
+        
+        res.status(201).end("The End!");
+
+      })
+
+
+
+
+      app.get("/seven", function(req,res){
+
+
+        res.cookie("name", "Serajum munira");
+        res.cookie("city", "Jhenidah");
+        res.cookie("age", "24 years old");
+        
+        res.end("Successfully Done!");
+
+      })
+
+
+
  app.listen(port, function(){
    console.log('Example app listening at http://localhost:' + port)
   })
