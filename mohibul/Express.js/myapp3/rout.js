927750b8e360a769c43c3e92a59505c0ea69137b
Mohibul Alam <mohibulalamjr@gmail.com>
2021-05-23 15:11:37 +0600

    oh

--- mohibul/Express.js/myapp3/rout.js
+++ mohibul/Express.js/myapp3/rout.js
@@ -3,10 +3,6 @@ const app = express();
  const port =8080;
 
 
-app.use('/',function(req,res,next){
-    console.log("I am application level middleware from home")
-    next();
-})
 
 
  app.get('/',function(req,res){
