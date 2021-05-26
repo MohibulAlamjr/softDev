c592dbc28f6d12b631a5736e48b2b0a43227b518
Md AL Kaium <kaium.github.cse@gmail.com>
2021-05-27 02:22:21 +0600

    boot

---
+++ kaium/React App/myapp/src/mycomponent/Arrow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const alrt =(a,b)=>{
+    alert(a+" "+b)
+}
+const Arrow = (props) => {
+    return(
+        <button className="btn btn-primary m-5">This is good</button>
+    );
+}
+
+export default Arrow;
