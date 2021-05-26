79366f16f6f4fbdc758959a5fd694951e892c917
Muniramouly <muniramouly25@gmail.com>
2021-05-26 09:05:21 +0600

    express

--- mouly/Reactjs/myfirstreact/src/App.js
+++ mouly/Reactjs/myfirstreact/src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './firstcomponents/Hello'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className = "App">
+     <h1>{1+4+7}</h1>
+     <h1>Serajum Munira</h1>
+
+     <Hello></Hello>
+
+
+   </div>
   );
 }
 
---
+++ mouly/Reactjs/myfirstreact/src/firstcomponents/Hello.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Hello(){
+    return (
+        <h1>Hello I am from Functional components</h1>
+    )
+}
+
+export default Hello;
