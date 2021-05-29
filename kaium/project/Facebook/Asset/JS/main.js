8c2bc16c4c55add62170b7eee7437cea3ff10959
Md AL Kaium <kaium.github.cse@gmail.com>
2021-05-29 11:36:21 +0600

    facebook

---
+++ kaium/project/Facebook/Asset/JS/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    for (i = new Date().getFullYear(); i > 1904; i--)
+{
+    $('#year').append($('<option />').html(i));
+}
+
+for (i = 1; i < 32; i++)
+{
+    $('#days').append($('<option />').html(i));
+}
+
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+for (i = 0 ; i < 13 ; i++)
+{
+    $('#months').append($('<option />').html(monthNames[i]));
+}
+
+})
