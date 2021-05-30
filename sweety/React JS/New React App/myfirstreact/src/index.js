870feedf6a6b2314ffe4a76d9c951a8cdd8ce428
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-05-31 02:39:55 +0600

    temp

--- sweety/React JS/New React App/myfirstreact/src/index.js
+++ sweety/React JS/New React App/myfirstreact/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import Portfolio from './Pages/Portfolio'
-import Style from './Pages/Style'
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Style from './Pages/Style';
 import {Route,Link,BrowserRouter as Router} from '../node_modules/react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/antd/dist/antd.css';
+import Slider from './my components/Slider';
 
 const myRoute = (
   <Router>
@@ -18,12 +20,14 @@ const myRoute = (
         <Link to = "/contact">Contact</Link>
         <Link to = "/portfolio">Portfolio</Link>
         <Link to = "/style">Style</Link>
+        <Link to = "/carousel">Slider</Link>
       </ul>
       <Route exact path = "/" component={App}/>
       <Route path = "/about" component={About}/>
       <Route path = "/contact" component={Contact}/>
       <Route path = "/portfolio" component={Portfolio}/>
       <Route path = "/style" component={Style}/>
+      <Route path = "/carousel" component={Slider}/>
     </div>
   </Router>
 )
