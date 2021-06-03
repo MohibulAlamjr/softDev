0a7dd0de8694e7bbaec397411ee4c4bff7475897
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-06-03 10:37:07 +0600

    Navbar

---
+++ sweety/React JS/New React App/myfirstreact/src/Pages/Tabledata.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ReactTable from "react-table-6";
+import '../../node_modules/react-table-6/react-table.css';
+import axios from '../../node_modules/axios';
+class Tabledata extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            mydata:[]
+        }
+    }
+    componentDidMount(){
+     axios.get('https://restcountries.eu/rest/v2/all').then(response=>{
+
+         this.setState({mydata:response.data})
+    }).catch(error=>{
+
+             console.log(error);
+         })
+     
+    }
+    render() {
+        const tableData=this.state.mydata;
+        const col=[{Header:"Name",accessor:"name"},{Header:"Capital",accessor:"capital"}]
+        return (
+            <div>
+                <ReactTable
+                data={tableData}
+                columns={col}
+                defaultPageSize={20}
+                pageSizeOptions={[2,4,6,10]}
+                
+                
+                />
+            </div>
+        );
+    }
+}
+
+export default Tabledata;
