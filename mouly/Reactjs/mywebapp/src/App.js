ea91caa260e95b686182229bf02e25b17478fb55
Muniramouly <muniramouly25@gmail.com>
2021-06-04 22:51:04 +0600

    rrr

--- mouly/Reactjs/mywebapp/src/App.js
+++ mouly/Reactjs/mywebapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Myroute from '../src/Component/Myroute';
 import Navbaarr from '../src/Component/Navbaarr';
 import {BrowserRouter}  from 'react-router-dom';
+import '../src/css/bootstrap.min.css';
 function App() {
   return (
     <div >
---
+++ mouly/Reactjs/mywebapp/src/Component/Pages/Logout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+
+class Logout extends Component {
+   
+
+    
+
+
+    render() {
+        sessionStorage.clear()
+       
+            return <Redirect to = "/login"/>
+        
+    }
+}
+
+export default Logout;
