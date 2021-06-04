ce138cdb5ff6506b13dab17dc6a553dbcd004db5
Muniramouly <muniramouly25@gmail.com>
2021-06-04 22:50:41 +0600

    bbb

--- mouly/Reactjs/mywebapp/src/Component/Myroute.js
+++ mouly/Reactjs/mywebapp/src/Component/Myroute.js
@@ -4,8 +4,9 @@ import Home from '../Component/Pages/Home';
 import About from '../Component/Pages/About';
 import Contact from '../Component/Pages/Contact';
 import Mywork from '../Component/Pages/Mywork';
-
-import Error from '../Component/Pages/Error'
+import Error from '../Component/Pages/Error';
+import Login from '../Component/Pages/Login';
+import Logout from '../Component/Pages/Logout';
 class Myroute extends Component {
     render() {
         return (
@@ -16,7 +17,10 @@ class Myroute extends Component {
       <Route path="/about" component={About} />
       <Route path="/contact/:username/:pass" component={Contact} />
       <Route path="/mywork" component={Mywork} />
+      <Route  path="/login" component={Login} />
+      <Route  path="/logout" component={Logout} />
       <Route  component={Error} />
+     
 
       </Switch>
             </div>
