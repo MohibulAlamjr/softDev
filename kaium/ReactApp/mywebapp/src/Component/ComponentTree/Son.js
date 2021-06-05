0a7dac3551ec7a14194bf01005952db2437a35f1
Md AL Kaium <kaium.github.cse@gmail.com>
2021-06-05 23:40:14 +0600

    react js

--- kaium/ReactApp/mywebapp/src/Component/ComponentTree/Son.js
+++ kaium/ReactApp/mywebapp/src/Component/ComponentTree/Son.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import Nati from './Nati'
 import Button from 'react-bootstrap/esm/Button';
 import { Redirect } from 'react-router';
-import Nati from './Nati';
+import {Dadaprovider} from './Mycontext';
 
 
 class Son extends Component {
@@ -12,9 +13,13 @@ class Son extends Component {
         }else{
             return (
                 <div>
-                    <Button className="btn btn-Success">Son</Button>
+                    <Dadaprovider value="Dada Sent Message To puti(Abba)">
+
+                         <Button className="btn btn-Success">Son</Button>
                     <Nati name="My name is Kaium"/>
                     
+                    </Dadaprovider>
+                   
                 </div>
             );
         }
