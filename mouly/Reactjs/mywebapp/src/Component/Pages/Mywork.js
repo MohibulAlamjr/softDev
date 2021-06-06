996fc901c555d1c4a17c651f44ee4387b0fd6ea4
Muniramouly <muniramouly25@gmail.com>
2021-06-06 16:06:26 +0600

    ttt

--- mouly/Reactjs/mywebapp/src/Component/Pages/Mywork.js
+++ mouly/Reactjs/mywebapp/src/Component/Pages/Mywork.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import Appheader from '../commonL/Appheader';
+
+const { Header, Content, Footer } = Layout;
 class Mywork extends Component {
     render() {
 
-        
+    
         if (sessionStorage.getItem("username")==null){
             return <Redirect to = "/login"/>
         }
@@ -11,8 +15,21 @@ class Mywork extends Component {
         else {
             return (
                 <div>
+                    <Layout className="mainlayout">
+    <Header>
+        <Appheader/>
+     
+    </Header>
+    </Layout>
+
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    
                     <h1>I am from mywork page</h1>
                 </div>
+                
             );
         }
        
