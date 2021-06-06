75b2f79d86d217a09660fc8645ce97b4857e8dcd
Md AL Kaium <kaium.github.cse@gmail.com>
2021-06-06 11:12:50 +0600

    react

--- kaium/ReactApp/mywebapp/src/Component/Component tree 2/Mycontext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Mycontext = React.createContext();
-const Dadaprovider = Mycontext.Provider
-const putiConsumer =Mycontext.Consumer
-
-export(Dadaprovider,putiConsumer)
