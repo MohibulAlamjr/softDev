afd5de4c8889dd8695f61e8a64de9df3477e292e
Mohibul Alam <mohibulalamjr@gmail.com>
2021-06-07 11:11:55 +0600

    new

--- mohibul/React.js/mywebapp/src/Component/Apppage/Mainhome.js
+++ mohibul/React.js/mywebapp/src/Component/Apppage/Mainhome.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Appabout from '../common/Appabout';
+import Appfaq from '../common/Appfaq';
 import Appfeature from '../common/Appfeature';
+import Apppricing from '../common/Apppricing';
 import Appwork from '../common/Appwork';
 import Hero from '../common/Hero';
 
@@ -12,6 +14,8 @@ class Mainhome extends Component {
                <Appabout></Appabout>
                <Appfeature></Appfeature>
                <Appwork></Appwork>
+               <Appfaq></Appfaq>
+               <Apppricing></Apppricing>
             </>
         );
     }
