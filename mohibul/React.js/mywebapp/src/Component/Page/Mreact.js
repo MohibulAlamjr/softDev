ab7fc9557a0f29c52b19692a4fbec4ee40e9352d
Mohibul Alam <mohibulalamjr@gmail.com>
2021-06-10 17:23:30 +0600

    mew

--- mohibul/React.js/mywebapp/src/Component/Page/Mreact.js
+++ mohibul/React.js/mywebapp/src/Component/Page/Mreact.js
@@ -3,9 +3,12 @@ import { Redirect } from 'react-router';
 import Customthem from '../metarial/Customthem';
 import Mcss from '../metarial/Mcss';
 import Typo from '../metarial/Typo';
-import {createMuiTheme,ThemeProvider} from '@material-ui/core';
+import { createMuiTheme,Drawer,ThemeProvider} from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import {  lightBlue, purple } from '@material-ui/core/colors';
+import Cardui from '../metarial/Cardui'
+import Layout from '../metarial/Layout';
+import Drawermu from '../metarial/Drawermu';
 
 
 const mycss=createMuiTheme({
@@ -30,6 +33,7 @@ const mycss=createMuiTheme({
 
 class Mreact extends Component {
     render() {
+        
 
         if (sessionStorage.getItem('userName')== null){
 
@@ -42,11 +46,15 @@ class Mreact extends Component {
              <ThemeProvider theme={mycss}>
                  <Container>
              <br/> <br/>
+             <Layout>
               <Typo/><br/> <br/>
               <Mcss/><br/> <br/>
+              
+              </Layout>
               <Customthem/><br/> <br/>
-          
-          
+              <Layout><Cardui/></Layout>
+              <Drawermu></Drawermu>
+              <Drawer></Drawer>
             </Container>
             </ThemeProvider>
             </>
