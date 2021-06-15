967b2c30d4b09bcff2378db83390fbcc2e9d13be
Muniramouly <muniramouly25@gmail.com>
2021-06-16 00:43:30 +0600

    yy

---
+++ mouly/jQuery/jproject3/asset/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+ function counter(id, start, end, duration) {
+  let obj = document.getElementById(id),
+   current = start,
+   range = end - start,
+   increment = end > start ? 1 : -1,
+   step = Math.abs(Math.floor(duration / range)),
+   timer = setInterval(() => {
+    current += increment;
+    obj.textContent = current;
+    if (current == end) {
+     clearInterval(timer);
+    }
+   }, step);
+ }
+ counter("count1", 0, 800);
+ counter("count2", 100, 900);
+ counter("count3", 0, 1050);
+});
